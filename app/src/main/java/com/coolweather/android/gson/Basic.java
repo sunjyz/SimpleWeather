072422e190e17f71caf1d0dde35169ed3fde19bd
sjy <sunjy00123@163.com>
2018-02-06 20:19:20 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2018/2/5.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public String weatherID;
+    public Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
